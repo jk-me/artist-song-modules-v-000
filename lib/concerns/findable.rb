caf036bfce6d1c0bd50c5b52c07523fc43a273a4
Jenny K <jkam17@gmail.com>
2018-08-29 17:59:11 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,9 @@
-  
+module Paramble
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
   
   def to_param
     name.downcase.gsub(' ', '-')
   end
+end
