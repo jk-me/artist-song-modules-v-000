a2eb6e34b49570a88fae5781b9e5f0827ce874a7
Jenny K <jkam17@gmail.com>
2018-08-29 18:21:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,8 +5,8 @@ class Artist
   attr_reader :songs
   @@artists = []
   
-  extend Memorable :: ClassMethods
-  include Memorable :: InstanceMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
